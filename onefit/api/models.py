ad41b78b489b5f59f55b87fff4fce75773a341e5
Adil <adilzhapar2002@gmail.com>
2023-01-25 13:59:00 +0600

    created user with authentication

--- onefit/api/models.py
+++ onefit/api/models.py
@@ -1,12 +1,80 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
 
 
-# Create your models here.
-class User(AbstractUser):
-    date_of_birth = models.DateField(blank=True)
+class MyUserManager(BaseUserManager):
+    def create_user(self, username, date_of_birth, password=None):
+        """
+        Creates and saves a User with the given username, date of
+        birth and password.
+        """
+        if not username:
+            raise ValueError('Users must have a username')
 
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'date_of_birth']
+        user = self.model(
+            username=username,
+            date_of_birth=date_of_birth,
+        )
 
-    def __str__(self):
-        return "{}".format(self.username)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, date_of_birth, password):
+        """
+        Creates and saves a superuser with the given username, date of
+        birth and password.
+        """
+        user = self.create_user(username,
+            password=password,
+            date_of_birth=date_of_birth
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=200, unique=True)
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    date_of_birth = models.DateField()
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['date_of_birth']
+
+    def get_full_name(self):
+        # The user is identified by their username address
+        return self.username
+
+    def get_short_name(self):
+        # The user is identified by their username address
+        return self.username
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
